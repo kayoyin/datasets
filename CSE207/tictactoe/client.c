a4a34f460b739ea29594123dcf84bc00cb988d8e
Kayo <kayo.yin@polytechnique.edu>
2019-06-03 19:31:14 +0200

    final push

---
+++ CSE207/tictactoe/client.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include "common.h"
+
+
+// Initial structure obtained from course moodle
+int main(int argc, char** argv) {
+    int terminate = 0;  // run until told otherwise
+    // Arguments parsing
+    if (argc<3) {
+        printf("Syntax: ./client <ipaddr> <port>\n");
+        exit(1);
+    }
+    int port = atoi(argv[2]);
+    struct sockaddr_in dest;
+    memset(&dest, 0, sizeof(struct sockaddr_in));
+    inet_pton(AF_INET, argv[1], &(dest.sin_addr));
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(port);
+    // Socket creation
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); // creation of datagram socket
+    if (sockfd < 0) {
+      perror("socket creation failed");
+      exit(EXIT_FAILURE);
+    }
+
+    // Greetings message
+    char *msg = "I want to play! \n";
+    if (sendto(sockfd, (const char *)msg, strlen(msg),
+        0, (const struct sockaddr *) &dest,
+            sizeof(dest)) < 0){
+              perror("greeting failed");
+    }
+
+    // Loop
+    char buffer[2048];
+    char msgtype;
+
+    while (! terminate) {
+        // Receive message from server
+        socklen_t len = sizeof(dest);
+        int n = recvfrom(sockfd, (char *)buffer, 2048,
+                0, (struct sockaddr *) &dest,
+                &len);
+
+        // Process message (and maybe interact with user? send message(s) to server?)
+        if (n == 0) break;
+
+        buffer[n] = '\0';
+        msgtype = buffer[0];
+        //show_bytes(buffer, sizeof(buffer));
+        if (msgtype == TXT){
+          printf("TXT message : %s\n", buffer);
+        }
+        else if (msgtype == FYI){
+          int npos = buffer[1];
+          printf("%d filled positions. \n", npos);
+          char board[3][3] = {{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
+          char player;
+          for (int i = 2; i < 3*npos+2; i += 3){
+            (buffer[i] == 1) ? (player = 'X') : (player = 'O');
+            board[(int)buffer[i+2]-1][(int)buffer[i+1]-1] = player;
+          }
+          printf("  c1  c2  c3 \n");
+          printf("r1 %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+          printf("  ---+---+-- \n");
+          printf("r2 %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+          printf("  ---+---+-- \n");
+          printf("r3 %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+        }
+        else if (msgtype == MYM){
+
+          while (1){
+            printf("Please choose your next move, syntax: row col (e.g. '2 1') \n");
+            int col, row;
+            scanf("%d %d", &row, &col);
+            /*if (!(isdigit(col) && isdigit(row))){
+              printf("Error: please input two integers 'row col' \n");
+              scanf("%d %d", &row, &col);
+            }*/
+            printf("You chose: (%d, %d) \n", row, col);
+            int bslen = 0;
+            msg = build_mov(col, row, &bslen);
+            printf("%s\n",msg);
+            break;
+          }
+          /*
+          sendto(sockfd, (const char *)msg, strlen(msg),
+              0, (const struct sockaddr *) &dest,
+                  sizeof(dest))
+          */
+          socklen_t destlen = sizeof(dest);
+          send_msg(MOV, sockfd, strlen(msg), msg, &dest, &destlen);
+
+        }
+        else if (msgtype == END)
+        {
+          if (buffer[1] == 0)
+          {
+            printf("The game is a tie! \n");
+          }
+          else
+          {
+            printf("Player %d is the winner! \n", buffer[1]);
+          }
+          terminate = 1;
+        }
+    }
+    // Terminate
+    close(sockfd);
+    return 0;
+}
---
+++ CSE207/tictactoe/server.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "common.h"
+
+int validate_move(int col, int row, Position** grid, int npos){
+  // return 0 if move not valid
+  if (col < 1 || col > 3 || row < 1 || row > 3){
+    return 0; // position not in 3x3 grid
+  }
+  for (int i = 0; i < npos; i++){
+    if (grid[i]->row == row && grid[i]->col == col){
+      return 0; // position taken already
+    }
+  }
+  return 1;
+}
+
+int check_winner(Position** grid, int npos){
+  // return -1 if not end, 1 if player 1 wins
+  // 2 if player 2 wins, 0 if tie
+  char lrow = grid[npos]->row;
+  char lcol = grid[npos]->col;
+  char lplay = grid[npos]->player;
+  char row;
+  char col;
+
+  int rows = 0;
+  int cols = 0;
+  int diag1 = 0;
+  int diag2 = 0;
+
+  for (int i = (int)lplay - 1; i < npos+1; i = i+2){
+    // iterate over positions current player occupies
+    // players are alternating so we can increment index by 2
+    row = grid[i]->row;
+    col = grid[i]->col;
+
+    if (row == lrow){
+      rows++;
+    }
+    if (col == lcol){
+      cols++;
+    }
+    if (lrow == lcol){
+      if (row == col){
+        diag1++;
+      }
+    }
+    if ((int)lrow + (int)lcol == 4){ // (1,3) (2,2) or (3,1)
+      if ((int)row + (int)col == 4){
+        diag2++;
+      }
+    }
+  }
+
+  if (rows == 3 || cols == 3 || diag1 == 3 || diag2 == 3){
+    return (int)lplay;
+  }
+
+  if (npos == 8){
+    return 0; // tie
+  }
+
+  return -1;
+}
+
+int main(int argc, char** argv) {
+    /* Start of code based on
+    https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+    */
+
+    struct Game game;
+
+    // Arguments parsing
+    if (argc<2) {
+        printf("Syntax: ./server <port>\n");
+        exit(1);
+    }
+    int port = atoi(argv[1]);
+    struct sockaddr_in servaddr, cliaddr;
+
+    memset(&servaddr, 0, sizeof(struct sockaddr_in));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    inet_pton(AF_INET, argv[1], &(servaddr.sin_addr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    // Socket creation
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); // creation of datagram socket
+    //int sockpl[2] = {0,0}; // socket for each player
+    if (sockfd < 0) {
+      perror("socket creation failed");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("Socket created.\n");
+
+
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+            sizeof(servaddr)) < 0 )
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Bind to port %d \n", port);
+    printf("Waiting for connections...\n");
+
+    char buffer[2048];
+    char symbols[2] = {'X', 'O'};
+    socklen_t len = sizeof(cliaddr);
+    char *msg;
+
+    game.nplayers = 0;
+    while (game.nplayers < 2){
+      int n = recvfrom(sockfd, (char *)buffer, 2048,
+                0, ( struct sockaddr *) &cliaddr,
+                &len);
+      buffer[n] = '\0';
+      printf("%s\n", buffer);
+      game.nplayers++;
+      printf("Player %d has connected \n", game.nplayers);
+
+      game.player[game.nplayers-1].socket = cliaddr;
+      game.player[game.nplayers-1].len_sock = len;
+      printf("Player %d assigned. \n", game.nplayers);
+
+      asprintf(&msg, " Welcome! You are player %d, you play with %c. \n", game.nplayers, symbols[game.nplayers-1]);
+      send_msg(TXT, sockfd, strlen(msg), msg, &cliaddr, &len);
+    }
+
+
+    game.active = 1;
+    game.turn = 0;
+    int bslen = 0;
+    game.npos = 0;
+    Position pos[9];
+    struct sockaddr_in psocket;
+    socklen_t psocklen;
+    char col,row;
+
+    while (game.active) {
+      psocket = game.player[game.turn].socket;
+      psocklen = game.player[game.turn].len_sock;
+      msg = build_fyi(game.grid, game.npos, &bslen);
+      send_msg(FYI, sockfd, strlen(msg), msg, &psocket, &psocklen);
+
+      int validmov = 0;
+      while (!validmov){
+        send_msg(MYM, sockfd, strlen(msg), msg, &psocket, &psocklen);
+        int n = recvfrom(sockfd, (char *)buffer, 2048,
+                  0, ( struct sockaddr *) &psocket, &psocklen);
+        buffer[n] = '\0';
+        col = (int)buffer[1];
+        row = (int)buffer[2];
+        validmov = validate_move(col,row, game.grid, game.npos);
+        if (!validmov){
+          msg = "Invalid move! Try again. \n";
+          send_msg(TXT, sockfd, strlen(msg), msg, &psocket, &psocklen);
+        }
+      }
+      printf("Received move is col=%d, row=%d. \n", col, row);
+      pos[game.npos].player = game.turn + 1;
+      pos[game.npos].col = col;
+      pos[game.npos].row = row;
+      game.grid[game.npos] = &pos[game.npos];
+
+      printf("Position inserted \n");
+
+      int res = check_winner(game.grid, game.npos);
+
+      if (res > -1){
+        msg = build_end(res, &bslen);
+        send_msg(END, sockfd, strlen(msg), msg, &psocket, &psocklen);
+        game.active = 0;
+      }
+
+      game.npos++;
+      game.turn = (game.turn + 1) % 2;
+    }
+    psocket = game.player[game.turn].socket;
+    psocklen = game.player[game.turn].len_sock;
+    send_msg(END, sockfd, strlen(msg), msg, &psocket, &psocklen);
+
+    close(sockfd);
+    return 0;
+}
